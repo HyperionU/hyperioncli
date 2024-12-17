9e988dd4cf72d317bcf0895a482d3094befabbca
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-12-17 00:13:04 +0000

    refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,17 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
-import { runCLI } from "./cli/index.js";
-import { cliFlags } from "./installers/index.js";
-import { getUserPkgManager } from "./utils/getPackageManager.js";
 import { Command } from "commander";
 import { version } from "../package.json";
+import { init } from "./command/init.js";
 
 
 const program = new Command()
-    .name("huCLI")
+    .name("hyperioncli")
     .description("The easiest way to configure your development environment to UofH Standards.")
     .version(chalk.grey(version))
 
 program
-    .option(
-        "--ntrx, --nitrox",
-        `Runs the new Nitrox bootstrapper. ${chalk.bgRedBright("EXPERIMENTAL")}`,
-        false
-    )
-    .option(
-        "-t, --turbo",
-        `Bootstrap a Turbo monorepo. ${chalk.bgRedBright("EXPERIMENTAL")}`,
-        false
-    )
-    .option(
-        "-y, --default",
-        "Skip the CLI and bootstrap a new environment using defaults.",
-        false
-    )
+    .addCommand(init)
 
-const flags: cliFlags = program.parse(process.argv).opts();
-
-const packageManager = getUserPkgManager()
-await runCLI(packageManager, flags);
+program.parse()
