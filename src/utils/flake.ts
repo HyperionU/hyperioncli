b02bd482c3028725af8407c30a9fb274b92d5f84
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-01 19:51:41 +0000

    adjust flake schema

--- src/utils/flake.ts
+++ src/utils/flake.ts
@@ -1,17 +1,18 @@
 import Ajv from "ajv";
-import * as schema from "./flake.schema.json"
-import { Flake } from "~/installers";
+import zodToJsonSchema from "zod-to-json-schema";
+import { Flake, flakeSchema } from "~/installers";
 
 const ajv = new Ajv()
 
-const validate = ajv.compile<Flake>(schema)
+const validate = ajv.compile<Flake>(zodToJsonSchema(flakeSchema))
 
 export const flakeValidate = async (flake: Flake) => {
-    const valid = validate(flake)
-    
-    if (!valid) {
-        console.error(validate.errors)
+    try {
+        const valid = validate(flake)
+        if (!valid) throw validate.errors
+        return valid;
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
     }
-
-    return valid;
 }
