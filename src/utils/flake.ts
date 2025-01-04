081a908f1e368e84328e5be36dcf0c383ea64726
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-04 03:10:58 +0000

    update schema ref

--- src/utils/flake.ts
+++ src/utils/flake.ts
@@ -30,7 +30,7 @@ export function generateFlakeSchema(fileName: string) {
 }
 
 const flakeDefault: Flake = {
-    $schema: "https://raw.githubusercontent.com/HyperionU/hyperioncli/refs/heads/exp-Flake/src/utils/schema/flake.schema.json",
+    $schema: "https://raw.githubusercontent.com/HyperionU/hyperioncli/refs/heads/exp-Flake/src/utils/schema/flake.json",
     description: "Example hyperioncli flake",
     packageManager: await getUserPkgManager(),
     services: {
