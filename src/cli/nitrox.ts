87602e771e637213f948ac48f4b083e1f0c3df7e
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-05 17:15:38 +0000

    patch nitrox docs initialisation

--- src/cli/nitrox.ts
+++ src/cli/nitrox.ts
@@ -7,40 +7,104 @@ import { nitroxStandardConfig } from "~/utils/prompts/nitrox/stdConfig";
 import { nitroxIntegrationConfig } from "~/utils/prompts/nitrox/integrationConfig";
 import { note } from "@clack/prompts";
 
-export const nitroxPrompt = async (packageManager: PackageManager, flags: cliFlags) => {
+export const nitroxPrompt = async (
+  packageManager: PackageManager,
+  flags: cliFlags
+) => {
+  note(
+    `Welcome to ${gradient.atlas("Nitrox")}. \nLet's get you up and running.`,
+    `${flags.turbo ? "Step 3a." : "Step 2a."}`
+  );
+  await setTimeout(1000);
 
-    note(`Welcome to ${gradient.atlas("Nitrox")}. \nLet's get you up and running.`, `${flags.turbo ? "Step 3a." : "Step 2a."}`);
-    await setTimeout(1000);
+  const config = await nitroxStandardConfig();
 
-    const config = await nitroxStandardConfig()
+  note(
+    "Now, let's add some integrations.",
+    `${flags.turbo ? "Step 3b." : "Step 2b."}`
+  );
+  await setTimeout(1000);
 
-    note("Now, let's add some integrations.", `${flags.turbo ? "Step 3b." : "Step 2b."}`)
-    await setTimeout(1000)
+  const integrations = await nitroxIntegrationConfig();
+  await runNitroxInit(packageManager, config, integrations, true);
+};
 
-    const integrations = await nitroxIntegrationConfig()
-    await runNitroxInit(packageManager, config, integrations, true);
+export async function runNitroxInit(
+  packageManager: string,
+  config: {
+    route: string;
+    typescript: string;
+    runInstall: boolean;
+    initGit: boolean;
+  },
+  integrations: (string | string[])[],
+  outFlag: boolean
+) {
+  switch (packageManager) {
+    case "npm":
+      await execa({
+        stdout: `${outFlag ? "inherit" : "ignore"}`,
+        stderr: `${outFlag ? "inherit" : "ignore"}`,
+      })`${packageManager} create astro ${
+        config.route
+      } -- --template minimal --typescript ${config.typescript} ${
+        config.runInstall ? "--install" : "--no-install"
+      } ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(
+        " "
+      )}`;
+      break;
+
+    default:
+      await execa({
+        stdout: `${outFlag ? "inherit" : "ignore"}`,
+        stderr: `${outFlag ? "inherit" : "ignore"}`,
+      })`${packageManager} create astro ${
+        config.route
+      } --template minimal --typescript ${config.typescript} ${
+        config.runInstall ? "--install" : "--no-install"
+      } ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(
+        " "
+      )}`;
+      break;
+  }
 }
 
-export async function runNitroxInit(packageManager: string, config: { route: string; typescript: string; runInstall: boolean; initGit: boolean; }, integrations: (string | string[])[], outFlag: boolean) {
-    switch (packageManager) {
-        case "npm":
-            await execa({ stdout: `${outFlag ? 'inherit' : 'ignore'}`, stderr: `${outFlag ? 'inherit' : 'ignore'}` })`${packageManager} create astro ${config.route} -- --template minimal --typescript ${config.typescript} ${config.runInstall ? "--install" : "--no-install"} ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(' ')}`;
-            break;
+export async function runStarlightInit(
+  packageManager: string,
+  config: {
+    route: string;
+    typescript: string;
+    runInstall: boolean;
+    initGit: boolean;
+  },
+  integrations: (string | string[])[],
+  outFlag: boolean
+) {
+  switch (packageManager) {
+    case "npm":
+      await execa({
+        stdout: `${outFlag ? "inherit" : "ignore"}`,
+        stderr: `${outFlag ? "inherit" : "ignore"}`,
+      })`${packageManager} create astro ${
+        config.route
+      } -- --template starlight --typescript ${config.typescript} ${
+        config.runInstall ? "--install" : "--no-install"
+      } ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(
+        " "
+      )}`;
+      break;
 
-        default:
-            await execa({ stdout: `${outFlag ? 'inherit' : 'ignore'}`, stderr: `${outFlag ? 'inherit' : 'ignore'}` })`${packageManager} create astro ${config.route} --template minimal --typescript ${config.typescript} ${config.runInstall ? "--install" : "--no-install"} ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(' ')}`;
-            break;
-    }
-}
-
-export async function runStarlightInit(packageManager: string, config: { route: string; typescript: string; runInstall: boolean; initGit: boolean; }, integrations: (string | string[])[], outFlag: boolean) {
-    switch (packageManager) {
-        case "npm":
-            await execa({ stdout: `${outFlag ? 'inherit' : 'ignore'}`, stderr: `${outFlag ? 'inherit' : 'ignore'}` })`${packageManager} create astro ${config.route} -- --template minimal --typescript ${config.typescript} ${config.runInstall ? "--install" : "--no-install"} ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(' ')}`;
-            break;
-
-        default:
-            await execa({ stdout: `${outFlag ? 'inherit' : 'ignore'}`, stderr: `${outFlag ? 'inherit' : 'ignore'}` })`${packageManager} create astro ${config.route} --template minimal --typescript ${config.typescript} ${config.runInstall ? "--install" : "--no-install"} ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(' ')}`;
-            break;
-    }
+    default:
+      await execa({
+        stdout: `${outFlag ? "inherit" : "ignore"}`,
+        stderr: `${outFlag ? "inherit" : "ignore"}`,
+      })`${packageManager} create astro ${
+        config.route
+      } --template starlight --typescript ${config.typescript} ${
+        config.runInstall ? "--install" : "--no-install"
+      } ${config.initGit ? "--git" : "--no-git"} --add ${integrations.join(
+        " "
+      )}`;
+      break;
+  }
 }
